e9ee18d41354dd34b64c7893342167e68bbc8d1c
marcallan34 <marc.allan34@gmail.com>
2017-06-11 12:55:55 -0500

    Create loops.js

--- loops.js
+++ loops.js
@@ -1,5 +1,4 @@
 function forLoop(array) {
-
 for (var i = 0; i < 25; i++) {
   if(i === 1) {
     array.push("I am 1 strange loop");
@@ -11,6 +10,8 @@ for (var i = 0; i < 25; i++) {
 return array;
 }
 
+
+
 function whileLoop(number){
   let countdown = number;
 
@@ -20,14 +21,16 @@ function whileLoop(number){
   return('done');
 }
 
+
+
 function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
-
 do {
   array.shift()
 } while (array.length > 0 && maybeTrue());
-
 return array
 }
+
+
